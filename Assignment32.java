de162f9a9c00e05c6ad113a3408166db0033ff20
shaikahmeda5 <shaikahmeda5@gmail.com>
2017-07-15 14:10:37 +0530

    Assignment-3.2-master

---
+++ Assignment32.java
@@ -0,0 +1,22 @@
+public class Assignment32 {
+	public static void main(String[] args) {
+		String s1 = "Harsh"; //Line 1
+		String s2 = "Harsh";//Line 2
+		System.out.println("s1 content : "+s1);
+		System.out.println("s2 content : "+s2);
+		System.out.println("s1==s2 : "+(s1==s2));
+        
+        
+        
+        String s3 = "Harsh"; //Line 3
+		String s4 = "Kavishwar";//Line 4
+		
+		System.out.println("s3 content : "+s3);
+		System.out.println("s4 content : "+s4);
+		System.out.println("s3==s4 : "+(s3==s4));
+        
+        
+        
+	}
+	
+} 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Assignment-3.2-master
+# Assignment-3.2
+Write a Java statement to assign the value true to the boolean variable same , when the String variable S1 has the same contents as the String variable S2 , and false otherwise.
